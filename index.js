f064e770c583ba39f9119ce3087855720c8bba17
hihi0980 <kinlam0608@gmail.com>
2022-02-11 10:59:39 +0800

    third? edit

--- index.js
+++ index.js
@@ -11,21 +11,9 @@ const client = new Discord.Client({
     ]
  });
 
-client.on('ready', () => {
+client.on('ready', async () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
-
-client.on('messageCreate',(message) =>{
-    if(message.content.startsWith('/n ') && message.content.length > 3){
-        message.reply(`https://nhentai.net/g/${message.content.slice(3)}`)
-    }
-    else if(message.content.startsWith('/18c ') && message.content.length > 5){
-        message.reply(`https://18comic.org/album/${message.content.slice(5)}`)
-    }
-    else if(message.content.startsWith('/w ') && message.content.length > 3){
-        message.reply(`https://www.wnacg.org/photos-index-aid-${message.content.slice(3)}.html`)
-    }
-})
 
 client.on("guildMemberAdd", async (member)=>{
     const img = await generateImage(member)
